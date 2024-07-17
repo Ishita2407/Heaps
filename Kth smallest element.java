c9c77570a2048559d9126d7bb851c139113d1bda
ISHITA <91796142+Ishita2407@users.noreply.github.com>
2024-07-17 16:25:40 +0530

    Create Kth smallest element.java

---
+++ Kth smallest element.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int findKthSmallest(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int num : nums) {
+            maxHeap.add(num);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        return maxHeap.peek();
+    }
+}
